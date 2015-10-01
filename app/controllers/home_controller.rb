23d3215ce6835138f6236b34877a1831de9c50bc
punst0531 <punst0531@gmail.com>
2015-10-01 05:47:41 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+    def index
+    
+    end
+    
+    def about
+    
+    end
+    
+    def profile
+    
+    end
+    
+    def contact
+        
+    end
+    
+    def Portfolio
+    
+    end
+        
+end
